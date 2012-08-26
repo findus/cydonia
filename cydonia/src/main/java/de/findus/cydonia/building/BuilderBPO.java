073f4040dd37f79985039c070c55162281bb73c8
Florian Kick <findus@munichlan.de>
2012-08-26 22:01:42 +0200

    building

--- cydonia/src/main/java/de/findus/cydonia/building/BuilderBPO.java
+++ cydonia/src/main/java/de/findus/cydonia/building/BuilderBPO.java
@@ -3,6 +3,9 @@
  */
 package de.findus.cydonia.building;
 
+import de.findus.cydonia.component.ComponentGroupEntity;
+
+
 /**
  * @author Findus
  *
@@ -10,17 +13,34 @@ package de.findus.cydonia.building;
 public class BuilderBPO {
 	
 	public void build(BuilderEntity builder) {
-		for(int y=0;y<3;y++) {
-			for(int x=0;x<3;x++) {
-				if(builder.getComponent(x, y) == null) {
-					continue;
+		if(!this.isConnected(builder) || builder.getComponentList().isEmpty()) {
+			return;
+		}
+		
+		int top = Integer.MAX_VALUE;
+		int bottom = 0;
+		int left = Integer.MAX_VALUE;
+		int right = 0;
+		
+		for(int x=0; x<builder.getSize().getWidth(); x++) {
+			for(int y=0; y<builder.getSize().getHeight(); y++) {
+				if(builder.getComponent(x, y) != null) {
+					if(y<top) top=y;
+					if(y>bottom) bottom=y;
+					if(x<left) left=x;
+					if(x>right) right=x;
 				}
-				
-				// TODO: baumsuche auf zusammenhang
-				
-				
 			}
 		}
+		
+		ComponentGroupEntity group = new ComponentGroupEntity();
+		
+		// TODO: Maße setzen und Komponenten kopieren.
+	}
+
+	private boolean isConnected(BuilderEntity builder) {
+		// TODO: implement
+		return true;
 	}
 
 }
--- cydonia/src/main/java/de/findus/cydonia/building/BuilderEntity.java
+++ cydonia/src/main/java/de/findus/cydonia/building/BuilderEntity.java
@@ -3,6 +3,10 @@
  */
 package de.findus.cydonia.building;
 
+import java.awt.Dimension;
+import java.util.Collection;
+import java.util.HashMap;
+
 import de.findus.cydonia.component.ComponentEntity;
 
 /**
@@ -10,22 +14,63 @@ import de.findus.cydonia.component.ComponentEntity;
  *
  */
 public class BuilderEntity {
-
-	private ComponentEntity[][] table;
+	
+	private HashMap<String, ComponentEntity> table;
+	
+	private Dimension size;
 	
 	/**
 	 * 
 	 */
 	public BuilderEntity() {
-		table = new ComponentEntity[3][3];
+		table = new HashMap<String, ComponentEntity>();
+		size = new Dimension(3, 3);
 	}
 	
+	/**
+	 * Inserts the given component at the specified field of this builders table.
+	 * If the specified coordinates are not valid (not in range) nothing is done.
+	 * If component is null, the object at the specified field is removed.
+	 * 
+	 * @param x x-coordinate of the field (horizontal from left; starting with 0)
+	 * @param y y-coordinate of the field (vertical from top; starting with 0)
+	 * @param component the component or null 
+	 */
 	public void setComponent(int x, int y, ComponentEntity component) {
-		table[x][y] = component;
+		if(x<0 || x>size.getWidth()) {
+			return;
+		}
+		if(y<0 || y>size.getHeight()) {
+			return ;
+		}
+		
+		if(component == null) {
+			table.remove("x" + x + "y" + y);
+		}
+		
+		table.put("x" + x + "y" + y, component);
 	}
 	
 	public ComponentEntity getComponent(int x, int y) {
-		return table[x][y];
+		return table.get("x" + x + "y" + y);
+	}
+	
+	public Collection<ComponentEntity> getComponentList() {
+		return table.values();
+	}
+
+	/**
+	 * @return the size
+	 */
+	public Dimension getSize() {
+		return size;
+	}
+
+	/**
+	 * @param size the size to set
+	 */
+	public void setSize(Dimension size) {
+		this.size = size;
 	}
 
 }
